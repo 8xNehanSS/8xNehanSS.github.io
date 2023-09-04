eb2c761a65aa9590f92f6f536975bc98fb310ce6
NehanIIT <nehan.sudasinghe@gmail.com>
2023-09-04 09:59:11 +0530

    responsive design

--- js/mainscript.js
+++ js/mainscript.js
@@ -44,6 +44,7 @@ const textToType = "8xNehanSS.."; // Replace with your desired text
 const typingSpeed = 200;
 
 window.addEventListener('load', function () {
+  windowSize();
 
   typeText(targetElement, textToType, typingSpeed);
 
@@ -52,3 +53,21 @@ window.addEventListener('load', function () {
       document.querySelector('#heading').style.display = 'flex';
   }, 3000);
 });
+
+function windowSize() {
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+  if(w<750) {
+    const targetElement = document.getElementById("nameheading");
+    const targetElement1 = document.getElementById("content1");
+    const targetElement2 = document.getElementById("line1");
+    const targetElement3 = document.getElementById("line2");
+    const targetElement4 = document.getElementById("whitespace");
+
+    targetElement.innerHTML = "N.S."
+    targetElement1.style.display = "block"
+    targetElement2.style.padding = "0px 5px 0px";
+    targetElement3.style.padding = "30px 5px 0px";
+    targetElement4.style.padding = "100px 0px 0px";
+  }
+}
